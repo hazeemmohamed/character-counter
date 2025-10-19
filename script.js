9d471a51d5faefe920388ce5cb172f8faaa4d9bd
hazeemmohamed <mhmmdhazeem9@gmail.com>
2025-10-19 09:06:19 +0300

    Updating the project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var inputArea = document.getElementById("inputArea")
 var currentCount = document.getElementById("currentCount")
-
+var warnMsg = document.querySelector(".warning")
+var btnClose = document.querySelector(".cls")
 
 function countChar(){
     var count = inputArea.value.length
@@ -11,8 +12,13 @@ function countChar(){
         
         inputArea.value = inputArea.value.substring(0, 200);
         count = 200;
-        alert("⚠️ You reached the maximum character limit!")
+        // alert("⚠️ You reached the maximum character limit!")
+        warnMsg.classList.remove("hidden")
     }
+
         currentCount.textContent = count
+        btnClose.addEventListener("click",function(){
+            warnMsg.classList.add("hidden")
+        })
 
 }
